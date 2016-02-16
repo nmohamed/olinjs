229c1222df7d9aba420cd1528806b9059fadf1eb
Nora Mohamed <jurukix@gmail.com>
2016-02-16 09:12:21 -0500

    added nonworking oauth

--- twot/app.js
+++ twot/app.js
@@ -6,10 +6,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var hbs = exphbs.create({defaultLayout: 'main'});
 
 var index = require('./routes/index');
-
-var hbs = exphbs.create({defaultLayout: 'main'});
+var auth = require('./auth');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/twotdb');
@@ -29,11 +31,62 @@ app.use(session({
   cookie: {}
 }));
 
+/* GOOGLE AUTHENTICATION */
+
+var auth = require('./auth');
+passport.use(new GoogleStrategy({
+    clientID: auth.clientID,
+    clientSecret: auth.clientSecret,
+    callbackURL: auth.callbackURL,
+    returnURL: 'localhost:3000/index'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+));
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({ secret: 'this is not a secret ;)',
+  resave: false,
+  saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/FAIL' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/SUCCESS');
+  });
+
+app.get('/user', ensureAuthenticated, function(req, res) {
+  res.send(req.user);
+})
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+    res.send(401);
+}
+
+/* END GOOGLE AUTHENTICATION*/
+
 app.get('/', index.loginPage);
 app.post('/login', index.login);
 app.get('/index', index.indexTwot);
 app.post('/delete', index.deleteTwot);
 app.post('/add', index.addTwot);
-
 
 app.listen(3000);
